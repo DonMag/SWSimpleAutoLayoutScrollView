f64e1cf078817df666d4603c4735783057f9f11e
DonMag <ACP@ACPs-iMac.local>
2016-12-02 09:30:12 -0500

    first commit

---
+++ SWSimpleAutoLayoutScrollView/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SWSimpleAutoLayoutScrollView
+//
+//  Created by Don Magnusson on 12/2/16.
+//  Copyright © 2016 DonMag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	// keep status bar visible in landscape orientation
+	override func prefersStatusBarHidden() -> Bool {
+		return false
+	}
+	
+}
+
